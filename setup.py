fcfed40c0e798e3ea5b5e75cbe149ce25a079cf5
Camys <76231670+cchapetzan@users.noreply.github.com>
2021-06-02 21:46:41 +0100

    Version 0.1 - NekoPy
    It's possible to get (almost) real time current and forecast weather return on CLI  via API.

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from setuptools import setup, find_packages
 
-def read_reqs():
-    with open('reqs.txt') as req:
-        content = req.read()
-        reqs = content.split('\n')
 
-    return reqs
+def read_requirements():
+    with open('requirements.txt') as req:
+        content = req.read()
+        requirements = content.split('\n')
+
+    return requirements
+
 
 setup(
     name='Nekopy',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=read_reqs(),
+    install_requires=read_requirements(),
     entry_points='''
         [console_scripts]
         Nekopy=Nekopy.cli:cli
